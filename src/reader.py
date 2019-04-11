5728be2ccd4f121f1cf3244db52a71d7664a78c6
apierro <antonio.pierro@gmail.com>
2019-04-11 12:50:16 +0200

    Added ether_gas_station method reader

--- src/reader.py
+++ src/reader.py
@@ -1,6 +1,6 @@
 import configparser
-import urllib.request
 import json
+from urllib.request import Request, urlopen
 
 
 class Reader:
@@ -12,22 +12,33 @@ class Reader:
         self.hash = '8f39fb4940c084460da00a876a521ef2ba84ad6ea8d2f5628c9f1f8aeb395342'
 
     def get_block(self):
-        with urllib.request.urlopen(self._get_block_api()) as url:
-            return json.loads(url.read().decode())
+        return json.loads(self._open_url(self._get_block_api()))
 
     def get_tx(self):
-        with urllib.request.urlopen(self._get_tx_api()) as url:
-            return json.loads(url.read().decode())
+        return json.loads(self._open_url(self._get_tx_api()))
 
     def get_txs_from_block(self):
         return self.get_block()['txids']
 
+    def get_gas_price(self):
+        return json.loads(self._open_url(self._get_ethgasstation_api()))
+
     def _get_block_api(self):
         return self.config['API']['block'] + str(self.block_id)
+
+    def _get_ethgasstation_api(self):
+        return self.config['API']['ethgasstation']
+
+    def _get_request(self, url):
+        return Request(url, headers={'User-Agent': 'Mozilla/5.0'})
 
     def _get_tx_api(self):
         return self.config['API']['tx'] + str(self.hash)
 
+    def _open_url(self, url):
+        return urlopen(self._get_request(url)).read()
+
 
 if __name__ == '__main__':
     r = Reader()
+    r.get_gas_price()
--- src/test_reader.py
+++ src/test_reader.py
@@ -9,13 +9,22 @@ class TestReader(unittest.TestCase):
         self.r = Reader()
 
     def test_get_block(self):
-        self.assertIsInstance(self.r.get_block(), dict)
+        block = self.r.get_block()
+        self.assertIsInstance(block, dict)
+        self.assertTrue('height' and 'fees' and 'txids' in block)
 
     def test_get_txs_from_block(self):
         self.assertIsInstance(self.r.get_txs_from_block(), list)
 
     def test_get_tx(self):
-        self.assertIsInstance(self.r.get_tx(), dict)
+        tx = self.r.get_tx()
+        self.assertIsInstance(tx, dict)
+        self.assertTrue('gas_limit' and 'gas_used' and 'gas_price' in tx)
+
+    def test_get_gas_price(self):
+        gas_price = self.r.get_gas_price()
+        self.assertIsInstance(gas_price, dict)
+        self.assertTrue('fast' and 'safeLow' and 'average' in gas_price)
 
 
 if __name__ == '__main__':
