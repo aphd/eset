2da18206556f1115725a886f65857a7655ea5e50
apierro <antonio.pierro@gmail.com>
2019-04-12 23:42:44 +0200

    removed output var from config.py

--- src/fetch.py
+++ src/fetch.py
@@ -9,20 +9,17 @@ class Fetch:
 
     def __init__(self):
         self.config = configparser.ConfigParser()
-        self.config.read('config.ini')
-
-        self.child_cls_name = self.__class__.__name__.replace('Fetch_', '')
-
-        self.output_dir = '/'.join([
-            self.config['FILE']['out_dir'],
-            self.child_cls_name
+        self.output = '-'.join([
+            'output',
+            self.__class__.__name__.replace('Fetch_', '')
         ])
-        if not os.path.exists(self.output_dir):
-            os.makedirs(self.output_dir)
+
+        self.config.read('config.ini')
+        if not os.path.exists(self.output):
+            os.makedirs(self.output)
 
     def curl(self, url, file_signature):
-        file_name = self.config['FILE']['out_dir'] + \
-            self._get_name_from_url(url, file_signature)
+        file_name = self._get_name_from_url(url, file_signature)
         url = url + self._get_random_token()
         subprocess.call([
             'curl', url, '-H',
@@ -33,7 +30,7 @@ class Fetch:
 
     def _get_name_from_url(self, url, file_signature):
         return '/'.join([
-            self.child_cls_name,
+            self.output,
             file_signature
         ])
 
